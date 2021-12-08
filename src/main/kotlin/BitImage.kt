818c662e35cf3deb002d6ac4372319379ae30b7c
Daniel Mangold <mangold.daniel@protonmail.com>
2021-12-08 23:26:33 +0100

    Remove unused functions and moved bitmap import to top-level function

--- src/main/kotlin/BitImage.kt
+++ src/main/kotlin/BitImage.kt
@@ -4,87 +4,59 @@ import kotlin.math.absoluteValue
 import kotlin.math.ceil
 import kotlin.math.pow
 
-@Suppress("MemberVisibilityCanBePrivate")
-class BitImage(val xSize: Int, val ySize: Int, val imageData: List<Byte>) {
+data class BitImage(val xSize: Int, val ySize: Int, val imageData: List<Byte>)
 
-    companion object Companion {
-        fun readFromBitmapFile(file: File): BitImage {
-            val fileBytes = file.readBytes()
+fun readFromBitmapFile(file: File): BitImage {
+    val fileBytes = file.readBytes()
 
-            fun read(offset: Int, length: Int = 4) = fileBytes.copyOfRange(offset, offset + length)
-            fun ByteArray.decodeToInt() = foldIndexed(0) { index, acc, byte ->
-                acc + 256.0.pow(index.toDouble()).toInt() * (byte.toInt() and 0xFF)
-            }
-            fun Int.makeDivisibleBy(n: Int) = this + (n - this % n) % n
+    fun read(offset: Int, length: Int = 4) = fileBytes.copyOfRange(offset, offset + length)
+    fun ByteArray.decodeToInt() = foldIndexed(0) { index, acc, byte ->
+        acc + 256.0.pow(index.toDouble()).toInt() * (byte.toInt() and 0xFF)
+    }
+    fun Int.makeDivisibleBy(n: Int) = this + (n - this % n) % n
 
-            // check magic number
-            if (!read(0, 2).contentEquals(byteArrayOf(0x42, 0x4D)))
-                error("File is not a Windows Bitmap")
-            // check color depth
-            if (read(28, 2).decodeToInt() != 1)
-                error("Bitmap must have a color depth of exactly 1")
-            // check compression
-            if (read(30).decodeToInt() != 0)
-                error("Bitmap must be uncompressed")
+    // check magic number
+    if (!read(0, 2).contentEquals(byteArrayOf(0x42, 0x4D)))
+        error("File is not a Windows Bitmap")
+    // check color depth
+    if (read(28, 2).decodeToInt() != 1)
+        error("Bitmap must have a color depth of exactly 1")
+    // check compression
+    if (read(30).decodeToInt() != 0)
+        error("Bitmap must be uncompressed")
 
-            val imageDataOffset = read(10).decodeToInt()
-            val bitmapWidth = read(18).decodeToInt()
-            val bitmapHeight = read(22).decodeToInt()
-            val imageData = read(imageDataOffset, fileBytes.size - imageDataOffset)
-                .toList()
-                .chunked(ceil(bitmapWidth / 8.0).toInt().makeDivisibleBy(4)) // padded to be divisible by 4
-                .let {
-                    val list = if (bitmapHeight > 0) it.asReversed().toMutableList() else it.toMutableList()
+    val imageDataOffset = read(10).decodeToInt()
+    val bitmapWidth = read(18).decodeToInt()
+    val bitmapHeight = read(22).decodeToInt()
+    val imageData = read(imageDataOffset, fileBytes.size - imageDataOffset)
+        .toList()
+        .chunked(ceil(bitmapWidth / 8.0).toInt().makeDivisibleBy(4)) // padded to be divisible by 4
+        .let {
+            val list = if (bitmapHeight > 0) it.asReversed().toMutableList() else it.toMutableList()
 
-                    for (i in list.size until list.size.makeDivisibleBy(8))
-                        list.add(List(list.first().size) { 0xFF.toByte() })
+            for (i in list.size until list.size.makeDivisibleBy(8))
+                list.add(List(list.first().size) { 0xFF.toByte() })
 
-                    list
-                }
-                .map { list -> list
-                    .take(bitmapWidth.makeDivisibleBy(8) / 8)
-                    .flatMap { byte -> List(8) { (128 shr it) and byte.toInt() != 0 } }
-                }
-                .chunked(8)
-                .map { list ->
-                    List(list.first().size) {
-                        var byte: Byte = 0
-
-                        for (i in 0 until 8)
-                            byte = byte.plus(if (list[i][it]) 128 shr i else 0).toByte()
-
-                        byte
-                    }
-                }
-                .let { list ->
-                    List(list.size * list.first().size) { list[it % list.size][it / list.size].inv() }
-                }
-
-            return BitImage(bitmapWidth.makeDivisibleBy(8) / 8, bitmapHeight.absoluteValue.makeDivisibleBy(8) / 8, imageData)
+            list
         }
-    }
+        .map { list -> list
+            .take(bitmapWidth.makeDivisibleBy(8) / 8)
+            .flatMap { byte -> List(8) { (128 shr it) and byte.toInt() != 0 } }
+        }
+        .chunked(8)
+        .map { list ->
+            List(list.first().size) {
+                var byte: Byte = 0
 
-    fun getDotRow(rowNumber: Int): List<Boolean> {
-        if (rowNumber !in 0 until (ySize * 8))
-            error("rowNumber must be in range ${0 until (ySize * 8)}, but was $rowNumber")
+                for (i in 0 until 8)
+                    byte = byte.plus(if (list[i][it]) 128 shr i else 0).toByte()
 
-        return imageData
-            .filterIndexed { index, _ -> index % ySize == rowNumber / 8 }
-            .map { byte -> byte.toInt() and (1 shl rowNumber) != 0 }
-    }
+                byte
+            }
+        }
+        .let { list ->
+            List(list.size * list.first().size) { list[it % list.size][it / list.size].inv() }
+        }
 
-    fun getDotColumn(columnNumber: Int): List<Boolean> {
-        if (columnNumber !in 0 until (xSize * 8))
-            error("columnNumber must be in range ${0 until (xSize * 8)}, but was $columnNumber")
-
-        return imageData
-            .filterIndexed { index, _ -> index / ySize == columnNumber }
-            .flatMap { byte -> List(8) { index -> (byte.toInt() and (128 shr index)) != 0 } }
-    }
-
-    fun getDotMatrix(): List<List<Boolean>> {
-        return imageData
-            .chunked(ySize)
-            .map { it.flatMap { byte -> List(8) { index -> (byte.toInt() and (128 shr index)) != 0 } } }
-    }
+    return BitImage(bitmapWidth.makeDivisibleBy(8) / 8, bitmapHeight.absoluteValue.makeDivisibleBy(8) / 8, imageData)
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import java.io.File
 fun main(args: Array<String>) {
     PrintBuilder()
         .setJustification(PrintBuilder.Justification.CENTER)
-        .defineBitImage(BitImage.readFromBitmapFile(File(args[0])))
+        .defineBitImage(readFromBitmapFile(File(args[0])))
         .printBitImage(PrintBuilder.BitImagePrintMode.QUADRUPLE)
         .cut()
         .executePrint(File("/dev/ttyUSB0"))

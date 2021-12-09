3c8dbd00bb1e474c5da3c3bf90006ad7c13b05ed
Daniel Mangold <mangold.daniel@protonmail.com>
2021-12-09 21:53:23 +0100

    Add documentation

--- src/main/kotlin/BitImage.kt
+++ src/main/kotlin/BitImage.kt
@@ -4,8 +4,27 @@ import kotlin.math.absoluteValue
 import kotlin.math.ceil
 import kotlin.math.pow
 
+/**
+ * This class represents a printable bit image with a total resolution of
+ * `xSize * ySize * 8`. `imageData` is ordered column first, so the first column of
+ * a bit image with a resolution of 1x1 would be exactly one byte (8 dots) and the
+ * complete image would be 8 bytes big.
+ * @param xSize     amount of pixels in horizontal direction divided by 8
+ * @param ySize     amount of pixels in vertical direction divided by 8
+ * @param imageData a list of bytes that define the image
+ */
 data class BitImage(val xSize: Int, val ySize: Int, val imageData: List<Byte>)
 
+/**
+ * Builds a printable bit image from a given bitmap image.
+ *
+ * The bitmap (windows bitmap, .bmp) must be monochromatic and uncompressed or
+ * else errors will be thrown. Bit images are limited to a certain resolution
+ * (see specification for details), depending on whether the image will be
+ * defined in the volatile or non-volatile (NV) storage of the printer.
+ * @param file the file to read the bitmap from
+ * @return a printable bit image
+ */
 fun readFromBitmapFile(file: File): BitImage {
     val fileBytes = file.readBytes()
 
